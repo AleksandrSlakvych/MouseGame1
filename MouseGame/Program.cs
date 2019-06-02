fc49d39963756c51ec247644704a21a55be1bc2b
Александр Слаквич <aleksandr.mxr@gmail.com>
2019-06-03 01:12:40 +0300

    1 ver

--- MouseGame/Program.cs
+++ MouseGame/Program.cs
@@ -1,3 +1,4 @@
+using NConsoleGraphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,16 +7,84 @@ using System.Threading.Tasks;
 
 namespace MouseGame
 {
+    public interface IUiComponent
+    {
+        void Draw(ConsoleGraphics g);
+    }
+
+    public class EventButtonArgs : EventArgs
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Button : IUiComponent
+    {
+        public int x, y;
+        public int sizeW = 30, sizeH = 30;
+
+        public Button(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Draw(ConsoleGraphics g)
+        {
+            if (Input.IsMouseLeftButtonDown)
+            {
+                OnLeftButtonClickOn?.Invoke(this.x, this.y);
+                OnLeftButtonClickMessage(this.x, this.y);
+                //OnLeftButtonClick?.Invoke(this, new EventButtonArgs { X = this.x, Y = this.y });
+            }
+            g.FillRectangle(0xff00ff00, x, y, sizeW, sizeH);
+        }
+
+        //public event EventHandler<EventButtonArgs> OnLeftButtonClick;
+
+        public event Func<int,int, int> OnLeftButtonClickOn;
+
+        public event Action<int, int> OnLeftButtonClickMessage;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            ConsoleGraphics g = new ConsoleGraphics();
+            List<IUiComponent> components = new List<IUiComponent>();
+            var firstbutton = new Button(10, 10);
+            var secondbutton = new Button(50, 50);
+            components.Add(firstbutton);
+            components.Add(secondbutton);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Func<int, int, int> func = (x, y) => { x = Input.MouseX; y = Input.MouseY; return x;  };
+
+            Action<int, int> action = (x, y) => { x = Input.MouseX; y = Input.MouseY;  };
+
+            firstbutton.OnLeftButtonClickMessage += action;
+            firstbutton.OnLeftButtonClickOn += func;
+
+            //firstbutton.OnLeftButtonClick += Firstbutton_OnLeftButtonClick;
+            while (true)
+            {
+                foreach (var component in components)
+                {
+                    component.Draw(g);
+                }
+
+
+
+                g.FlipPages();
+            }
+
+        }
+
+
+        private static void Firstbutton_OnLeftButtonClick(object sender, EventButtonArgs eventButton)
+        {
+            eventButton.X += 10;
+            eventButton.Y += 10;
         }
     }
+
 }
